0099710de8d4070acac20c0d7bbadf47cb70f3f3
cvenanzi <phalaena.tobirea@gmail.com>
2023-11-12 00:48:00 +0100

    exposes some Ingredient endpoint (update still not working) and adds some notes

--- src/main/kotlin/com/chiara/exercises/cornucopia/rest/WelcomeController.kt
+++ src/main/kotlin/com/chiara/exercises/cornucopia/rest/WelcomeController.kt
@@ -1,7 +1,11 @@
 package com.chiara.exercises.cornucopia.rest
 
+import com.chiara.exercises.cornucopia.entity.Ingredient
 import com.chiara.exercises.cornucopia.service.IngredientService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,6 +19,15 @@ class WelcomeController (
     fun welcome() = "Welcome to Cornucopia, banquet of dreams, delight of the aggrieved"
 
     @GetMapping("/ingredients")
-    fun ingredients() = ingredientService.findAllIngredients()
+    fun getAllIngredients() = ingredientService.findAllIngredients()
+
+    @GetMapping("/ingredient/{id}")
+    fun getIngredient(@PathVariable id : Long) = ingredientService.findIngredientById(id)
+
+    @PostMapping("ingredient/update/{id}")
+    fun updateIngredient(@PathVariable id : Long, @RequestBody ingredient: Ingredient) {
+        println(ingredient)
+        ingredientService.updateIngredientById(id, ingredient)
+    }
 
 }
