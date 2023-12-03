cfd95119dce53b1ed30e30ec3e57da7b5ba7b99b
cvenanzi <phalaena.tobirea@gmail.com>
2023-12-03 12:35:11 +0100

    chapter-1: adds logger to ingredient controller

--- src/main/kotlin/com/chiara/exercises/cornucopia/rest/IngredientController.kt
+++ src/main/kotlin/com/chiara/exercises/cornucopia/rest/IngredientController.kt
@@ -3,12 +3,15 @@ package com.chiara.exercises.cornucopia.rest
 import com.chiara.exercises.cornucopia.entity.Ingredient
 import com.chiara.exercises.cornucopia.error.exception.FailedSaveException
 import com.chiara.exercises.cornucopia.service.IngredientService
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+
+private val logger = KotlinLogging.logger {}
 
 @RequestMapping("/ingredient")
 @RestController
@@ -23,13 +26,15 @@ class IngredientController (
 
     @PostMapping("/update/{id}")
     fun updateIngredient(@PathVariable id : Long, @RequestBody ingredient: Ingredient) : Ingredient {
+        logger.info { "accessed UPDATE" }
         println(ingredient)
         ingredientService.updateIngredientById(id, ingredient)
         return ingredient
     }
 
-    @PostMapping("save")
+    @PostMapping("/save")
     fun saveIngredient(@RequestBody ingredient: Ingredient) : Long {
+        logger.info { "accessed SAVE" }
         return try {
             ingredientService.saveIngredient(ingredient)!!
         } catch (e: Exception) {
