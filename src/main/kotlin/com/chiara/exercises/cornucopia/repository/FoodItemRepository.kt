295465924621071f0cf164b1bd4cb7e2c42b731e
cvenanzi <phalaena.tobirea@gmail.com>
2023-12-25 23:28:39 +0100

    chapter-2: minor addition

--- src/main/kotlin/com/chiara/exercises/cornucopia/repository/FoodItemRepository.kt
+++ src/main/kotlin/com/chiara/exercises/cornucopia/repository/FoodItemRepository.kt
@@ -2,6 +2,7 @@ package com.chiara.exercises.cornucopia.repository
 
 import com.chiara.exercises.cornucopia.entity.FoodItem
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-//@RepositoryRestResource(path = "food_items")
+@RepositoryRestResource(path = "food_items")
 interface FoodItemRepository : JpaRepository<FoodItem, Long>
