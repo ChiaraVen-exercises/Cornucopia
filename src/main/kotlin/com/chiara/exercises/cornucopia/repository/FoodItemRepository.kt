edb042be4f49a3e64d92fcb422a1bf61d4cb4508
cvenanzi <phalaena.tobirea@gmail.com>
2023-12-25 23:07:38 +0100

    chapter-2: modifies base url of two auto-generated controllers

--- src/main/kotlin/com/chiara/exercises/cornucopia/repository/FoodItemRepository.kt
+++ src/main/kotlin/com/chiara/exercises/cornucopia/repository/FoodItemRepository.kt
@@ -2,5 +2,7 @@ package com.chiara.exercises.cornucopia.repository
 
 import com.chiara.exercises.cornucopia.entity.FoodItem
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
+@RepositoryRestResource(path = "food_items")
 interface FoodItemRepository : CrudRepository<FoodItem, Long>
--- src/main/kotlin/com/chiara/exercises/cornucopia/repository/FoodLotRepository.kt
+++ src/main/kotlin/com/chiara/exercises/cornucopia/repository/FoodLotRepository.kt
@@ -2,5 +2,7 @@ package com.chiara.exercises.cornucopia.repository
 
 import com.chiara.exercises.cornucopia.entity.FoodLot
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
+@RepositoryRestResource(path = "food_lots")
 interface FoodLotRepository : CrudRepository<FoodLot, Long>
